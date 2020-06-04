ce76f6d443da5f9648ca4f55beb70e59da5e0a56
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-04 02:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
-  document.getElementById("text").textContent = "This is really cool!"
+  document.getElementById("text").textContent = "This is really cool!";
 });
